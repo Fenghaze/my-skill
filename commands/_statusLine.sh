2f6069b898e58d8a47b765181ae8e1694a79b60c
fenghaze <feng_haze@163.com>
2026-04-05 15:25:46 +0800

    Sync skills and commands - 2026-04-05 15:25:46

--- commands/_statusLine.sh
+++ commands/_statusLine.sh
@@ -30,7 +30,16 @@ try:
 
     context_window = data.get('context_window', {})
     remaining = context_window.get('remaining_percentage')
-    context_info = f'{remaining:.0f}%' if remaining is not None else '100%'
+    context_pct = remaining if remaining is not None else 100
+    context_info = f'{context_pct:.0f}%'
+
+    # 根据剩余上下文选择颜色
+    if context_pct > 50:
+        ctx_color = '32'   # 绿色 - 充裕
+    elif context_pct > 20:
+        ctx_color = '33'   # 黄色 - 警告
+    else:
+        ctx_color = '31'   # 红色 - 紧张
 
     output_style = data.get('output_style', {})
     style_name = output_style.get('name') if isinstance(output_style, dict) else None
@@ -40,7 +49,7 @@ try:
     tool_mode = data.get('permission_mode', 'default')
     mode_label = MODE_LABELS.get(tool_mode, tool_mode)
 
-    print(f'\033[35m@{username}\033[0m \033[36m{cwd_display}\033[0m:[\033[34m{model_display}\033[0m|\033[33m{style_name}\033[0m|\033[32m{mode_label}\033[0m]\033[31m({context_info})\033[0m')
+    print(f'\033[35m@{username}\033[0m \033[36m{cwd_display}\033[0m:[\033[34m{model_display}\033[0m|\033[33m{style_name}\033[0m|\033[32m{mode_label}\033[0m]\033[{ctx_color}m({context_info})\033[0m')
 
 except Exception:
     print(f'\033[31m@?\033[0m \033[36m?\033[0m:[\033[31mError\033[0m|\033[35m?\033[0m|\033[31m?\033[0m]\033[33m(100%)\033[0m')
